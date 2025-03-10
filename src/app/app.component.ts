0a17a83e43049008471ffac585d6f83df7a8896c
Afan Dayu Laksono <afan.laksono_ext@allobank.com>
2025-03-10 14:00:41 +0700

    feat: adding aside content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MyInfoComponent } from './components/my-info/my-info.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MyInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: AboutComponent },
+  // { path: '', component: AboutComponent },
+  // { path: '', component: AboutComponent },
+];
