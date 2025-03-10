4bebbc9c48d9b2e22ef860bf38f8c3edbcbd98c1
Afan Dayu Laksono <afan.laksono_ext@allobank.com>
2025-03-10 14:20:50 +0700

    feat: adding router active

--- src/app/components/my-info/my-info.component.ts
+++ src/app/components/my-info/my-info.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-my-info',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './my-info.component.html',
   styleUrl: './my-info.component.css'
 })
