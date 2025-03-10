adeee95223a41c61c3a82f5507a2a8e6db8ad2db
Afan Dayu Laksono <afan.laksono_ext@allobank.com>
2025-03-10 14:09:19 +0700

    feat: create components, edit description

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 
 export const routes: Routes = [
   { path: '', component: AboutComponent },
-  // { path: '', component: AboutComponent },
-  // { path: '', component: AboutComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'projects', component: ProjectsComponent },
 ];
